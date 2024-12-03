c04ee2628fb992a0e33110df691646cba90546f2
Prem-Kumar-Gupta <premgupta1629@gmail.com>
2024-12-03 20:47:35 +0530

    finishing css and logic

--- Componenets/Gallery.jsx
+++ Componenets/Gallery.jsx
@@ -1,7 +1,49 @@
-export default function Gallery(){
-return(
-    <>
-    <div>hi</div>
-    </>
-)
-} 
+import React, { useRef, useEffect } from "react";
+import Slides from "./Sildes";
+import image1 from "../Assets/image1.png";
+import image2 from "../Assets/image-2.png";
+import image3 from "../Assets/image-3.png";
+import image4 from "../Assets/image-4.png";
+import image5 from "../Assets/image-5.png";
+import image6 from "../Assets/image-6.png";
+
+
+export default function Gallery() {
+  const scrollContainer = useRef(null);
+
+  useEffect(() => {
+    const container = scrollContainer.current;
+    if (!container) return;
+    container.addEventListener("wheel", (e) => {
+      e.preventDefault();
+      container.scrollLeft += e.deltaY;
+    });
+  }, []);
+
+  return (
+    <div className="gallery-wrap">
+      <div ref={scrollContainer} className="gallery">
+        <div>
+          <span>
+            <Slides imgUrl={image1} />
+          </span>
+          <span>
+            <Slides imgUrl={image2} />
+          </span>
+          <span>
+            <Slides imgUrl={image3} />
+          </span>
+          <span>
+            <Slides imgUrl={image4} />
+          </span>
+          <span>
+            <Slides imgUrl={image5} />
+          </span>
+          <span>
+            <Slides imgUrl={image6} />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ Componenets/Sildes.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Sildes({imgUrl}) {
+  return (
+    <>
+    <img src={imgUrl} alt="" />
+    </>
+  )
+}
